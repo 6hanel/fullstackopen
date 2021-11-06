484f51745283c5ed41ff6edf0605ab7a54ec69fa
Lynn <89677103+6hanel@users.noreply.github.com>
2021-11-06 15:35:03 +0800

    Add files via upload

---
+++ part2/2.5 Course information/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Course from './Course'
+
+const App = ({courses}) => {
+  
+  return (
+    <>
+    <Course courses = {courses}/>
+    </>
+  )
+}
+export default App
---
+++ part2/2.5 Course information/src/Course.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+  const Course = ({courses}) => {
+
+    const Header = (props) => {
+      return <h1>{props.name}</h1>
+    }
+  
+    const Content = (props) => {
+      const content = 
+        props.part.map(course => <div key={courses.id} note={courses}>{course.name} {course.exercises}<br/><br/></div>)
+      return <>{content}</>
+    }
+  
+    const Total = (props) => {
+      const total = 
+        props.parts.reduce((sum, course) => sum + course.exercises, 0)
+      return <b>total of {total} exercises</b>;
+    }
+
+    return(
+    <>
+    <Header name = {courses[0].name}/>
+    <Content part = {courses[0].parts}/>
+    <Total parts={courses[0].parts}/>
+    <Header name = {courses[1].name}/>
+    <Content part = {courses[1].parts}/>
+    <Total parts={courses[1].parts}/>
+    </>
+    )
+  }
+  export default Course
